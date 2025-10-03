b225fdbf15743257682ecaa9fb7c9fa73fde88ef
NOOLFJ <firdavsjuraev183@gmail.com>
2025-10-03 16:47:49 +0300

    Fix image on (server)CLOUDE

--- myshop/urls.py
+++ myshop/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),  # <- корень
+    path('', TemplateView.as_view(template_name='index.html')), 
     path('admin/', admin.site.urls),
     path('api/', include('products.urls')),
 ]
